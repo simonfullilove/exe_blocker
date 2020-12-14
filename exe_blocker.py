f88d5e9c07efdfc9420855f99cac2f52b2c01647
Simon Fullilove <simonx49@hotmail.com>
2020-12-14 20:57:21 +0000

    updating code to add ui

--- exe_blocker.py
+++ exe_blocker.py
@@ -1,29 +1,52 @@
+from tkinter import filedialog
 import os
 # shell required to run os commands as admin
 import win32com.shell.shell as shell
+import tkinter as tk
 
-runfileyn = 'x'
-while runfileyn != '':
-	path = input('Enter a directory path in which to block executable files: ')
+root = tk.Tk()
+root.title('Exe Blocker')
 
-	# crawls path directory and creates array of executables found in it and subdirectories
-	exes = []
-	for folder in os.walk(path):
-		for found_file in folder[2]:
-			if found_file[-4:].lower() == '.exe':
-				exes.append(folder[0] + '\\' + found_file)
+canvas1 = tk.Canvas(root, width=300, height=0)
+canvas1.pack()
 
-	# if any exes found, creates a one line cmd command creating rule for each exe
-	if len(exes) > 0:
-		command = ''
-		for exe in exes:
-			print('Executable found:', exe)
-			if command != '':
-				command += '&&'
-			command += 'netsh advfirewall firewall add rule name="BlockProgram" dir=out program="' + exe + '" profile=any action=block'
+def block_exes(path):
+    # crawls path directory and creates array of executables found in it and subdirectories
+    exes = []
+    for folder in os.walk(path):
+        for found_file in folder[2]:
+            if found_file[-4:].lower() == '.exe':
+                fullpath = folder[0] + '\\' + found_file
+                exes.append(fullpath)
+                output.insert(tk.END, 'Executable found: ' + fullpath + '\n')
+                output.see(tk.END)
+    # if any exes found, creates a one line cmd command creating rule for each exe
+    if len(exes) > 0:
+        command = ''
+        for exe in exes:
+            if command != '':
+                command += '&&'
+            command += 'netsh advfirewall firewall add rule name="BlockProgram" dir=out program="' + exe + '" profile=any action=block'
+        # runs command as admin
+        shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + command)
+    output.insert(tk.END, str(len(exes)) + ' files blocked!' + '\n')
+    output.see(tk.END)
 
-		# runs command as admin
-		shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+command)
+def choose_directory():
+    dirpath = filedialog.askdirectory(title='Choose a folder')
+    dirpath = dirpath.replace('/','\\')
+    block_exes(dirpath)
 
-	print(len(exes), 'files blocked!')
-	runfileyn = input('Press Enter to quit or any key to continue: ')
+button1 = tk.Button(text='Select Directory...', command=choose_directory, bg='white', fg='black')
+button1.pack(padx = 5, anchor='nw')
+
+canvas1 = tk.Canvas(root, width=300, height=0)
+canvas1.pack()
+
+output = tk.Text(root,width=120,height=30, bg='white', fg='black')
+output.pack()
+
+label1 = tk.Label(root, text="Exe Blocker v1.0")
+label1.pack()
+
+root.mainloop()
